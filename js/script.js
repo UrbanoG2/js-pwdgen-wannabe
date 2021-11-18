76dc2cf41dae47e396d655c79a0f5593c1e83936
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-11-18 15:49:38 +0100

    done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@
 
 
 // chiediamo all'utente il suo nome
+
+let name = prompt ("Come ti chiami?");
+console.log(name);
+
+// chiediamo all'utente il suo cognome
+
+let surname = prompt ("Qual è il tuo cognome?");
+console.log(surname);
+
+// chiediamo all'utente il suo colore preferito
+
+let color = prompt ("Qual è il tuo colore preferito?");
+console.log(color);
+
+// anno attuale
+
+let year = "21"
+
+// scriviamo sulla pagina il risultato
+
+document.writeln (name+surname+color+year);
+console.log(name+surname+color+year);
+
+
+// scriviamo nel div di riferimento
+
+document.getElementById("random_id").innerHTML = name+surname+color+year;
+console.log(name+surname+color+year);
